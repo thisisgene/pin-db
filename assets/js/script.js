4b1f1c8faf58687ab8418d4eb16dc016dec98938
thisisgene <emdo2000@gmail.com>
2017-08-02 00:01:19 +0200

    Made table searchable.

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,29 @@ function loadCity() {
   }
 
 }
+
+function filterDb(col) {
+  // Declare variables
+  var id, input, filter, table, tr, td, i;
+  id = "input-" + col;
+  id = id.toString();
+  console.log(id);
+  input = document.getElementById(id);
+  filter = input.value.toUpperCase();
+  table = document.getElementById("data-table");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[col];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+        console.log('here');
+      } else {
+        tr[i].style.display = "none";
+        console.log('not here');
+      }
+    }
+  }
+}
